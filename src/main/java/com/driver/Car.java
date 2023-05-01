77853864ae456fce9e1ae34a41046a4a22a46a07
unknown <polishettianusha@gmail.com>
2023-05-01 14:27:24 +0530

    Submission

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -1,5 +1,8 @@
 package com.driver;
 
+
+import java.util.*;
+
 public class Car extends Vehicle {
     private int wheels;
     private String type;
@@ -11,15 +14,26 @@ public class Car extends Vehicle {
 
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
         //Hint: Car extends Vehicle
+        super(name);
+        this.wheels = wheels;
+        this.doors = doors;
+        this.gears = gears;
+        this.isManual = isManual;
+        this.type = type;
+        this.seats = seats;
+        this.currentGear = 1;
+
     }
 
     public void changeGear(int newGear){
+        currentGear = newGear;
 
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
 
+        move(newSpeed, newDirection);//prent class function. we can call using super.move() as well                                                                                            m
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
 }
